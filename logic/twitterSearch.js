ad5b9a36fa18e833d0b08fd871b15dfd7c34de7d
silver-io <javier@silver.io>
2014-10-15 01:08:19 +0100

    client side development completed. add database

---
+++ logic/twitterSearch.js
@@ -0,0 +1,30 @@
+//includes
+var util = require('util'),
+    twitter = require('twitter'),
+    sentimentAnalysis = require('./sentimentAnalysis'),
+    db = require('diskdb');
+
+db = db.connect('db', ['sentiments']);
+//config
+
+
+module.exports = function(text, callback) {
+  var twitterClient = new twitter(config);
+  var response = [], dbData = []; // to store the tweets and sentiment
+
+  twitterClient.search(text, function(data) {
+    for (var i = 0; i < data.statuses.length; i++) {
+      var resp = {};
+
+      resp.tweet = data.statuses[i];
+      resp.sentiment = sentimentAnalysis(data.statuses[i].text);
+      dbData.push({
+        tweet: resp.tweet.text,
+        score: resp.sentiment.score
+      });
+      response.push(resp);
+    };
+    db.sentiments.save(dbData);
+    callback(response);
+  });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+var twitterSearch = require('../logic/twitterSearch');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/data', function(req, res) {
+  res.json(require('diskdb')
+    .connect('db', ['sentiments'])
+    .sentiments.find());
+});
+
+router.post('/search', function(req, res) {
+  twitterSearch(req.body.search, function (data) {
+    res.json(data);
+  });
 });
 
 module.exports = router;
